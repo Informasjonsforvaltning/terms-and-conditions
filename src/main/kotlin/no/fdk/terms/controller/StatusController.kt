3609641fa35972b75bbd289acc759730887f2572
NilsOveTen <nils.ove.tendenes@acando.no>
2020-05-25 13:04:20 +0200

    chore: allow cors

--- src/main/kotlin/no/fdk/terms/controller/StatusController.kt
+++ src/main/kotlin/no/fdk/terms/controller/StatusController.kt
@@ -3,9 +3,12 @@ package no.fdk.terms.controller
 import no.fdk.terms.service.TermsService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+
+@CrossOrigin
 @RestController
 class StatusController (
     private val termsService: TermsService
