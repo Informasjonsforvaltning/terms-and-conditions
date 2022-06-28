059113118b9190b25527e16194af45593c09b22d
NilsOveTen <github.nils.ove@tendenes.com>
2022-06-28 09:24:06 +0200

    chore: replace WebSecurityConfigurerAdapter with SecurityFilterChain

--- src/main/kotlin/no/fdk/terms/Application.kt
+++ src/main/kotlin/no/fdk/terms/Application.kt
@@ -4,9 +4,10 @@ import no.fdk.terms.security.SecurityProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
 
 @SpringBootApplication
-@EnableConfigurationProperties(SecurityProperties::class)
+@EnableConfigurationProperties(SecurityProperties::class, OAuth2ResourceServerProperties::class)
 open class Application
 
 fun main(args: Array<String>) {
--- src/main/kotlin/no/fdk/terms/security/SecurityConfig.kt
+++ src/main/kotlin/no/fdk/terms/security/SecurityConfig.kt
@@ -2,39 +2,34 @@ package no.fdk.terms.security
 
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
-import org.springframework.security.oauth2.jwt.*
 import org.springframework.security.oauth2.jwt.JwtClaimNames.AUD
+import org.springframework.security.oauth2.jwt.JwtClaimValidator
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.JwtIssuerValidator
+import org.springframework.security.oauth2.jwt.JwtTimestampValidator
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
+import org.springframework.security.web.SecurityFilterChain
 
-@Configuration
-open class SecurityConfig : WebSecurityConfigurerAdapter() {
-    override fun configure(http: HttpSecurity) {
-        http.csrf().disable()
-
-        http.cors()
-            .and()
-                .authorizeRequests()
-                    .antMatchers(HttpMethod.OPTIONS)
-                        .permitAll()
-                    .antMatchers(HttpMethod.GET, "/terms/org/**/version")
-                        .permitAll()
-                    .antMatchers(HttpMethod.GET, "/terms/org/**")
-                        .authenticated()
-                    .antMatchers(HttpMethod.GET)
-                        .permitAll()
-                    .anyRequest()
-                        .authenticated()
-            .and()
-                .oauth2ResourceServer()
-                    .jwt()
+@EnableWebSecurity
+open class SecurityConfig {
+    @Bean
+    open fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeRequests{ authorize ->
+                authorize.antMatchers(HttpMethod.OPTIONS).permitAll()
+                    .antMatchers(HttpMethod.GET, "/terms/org/**/version").permitAll()
+                    .antMatchers(HttpMethod.GET, "/terms/org/**").authenticated()
+                    .antMatchers(HttpMethod.GET).permitAll()
+                    .anyRequest().authenticated() }
+            .oauth2ResourceServer { resourceServer -> resourceServer.jwt() }
+        return http.build()
     }
 
     @Bean
-    open fun jwtDecoder(properties: OAuth2ResourceServerProperties): JwtDecoder? {
+    open fun jwtDecoder(properties: OAuth2ResourceServerProperties): JwtDecoder {
         val jwtDecoder = NimbusJwtDecoder.withJwkSetUri(properties.jwt.jwkSetUri).build()
         jwtDecoder.setJwtValidator(
                 DelegatingOAuth2TokenValidator(
